526f2e32d1835d15c6411c62a17bd6d688b8d224
B3none <ablackham2000@gmail.com>
2019-05-14 09:04:52 +0100

    add new twitch things to env

--- env.example.php
+++ env.example.php
@@ -7,8 +7,8 @@ $variables = [
     'DB_NAME' => '', // Your database name.
     'STEAM_API_KEY' => '', // Your steam API key.
     'BASE_TITLE' => 'B3none', // This is the base title for your site.
-    '' => '',
-
+    'TWITCH_CLIENT_ID' => '',
+    'TWITCH_CLIENT_SECRET' => '',
 
     // Core configs
     'MATCHES_PAGE_LIMIT' => 10, // Page limit for matches page.
